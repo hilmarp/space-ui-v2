e80bafe5b224759eb062db9e7d18977fec6a64ed
Hilmar <hilmar90@gmail.com>
2022-03-12 16:58:32 +0000

    add analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,26 @@
+import Script from 'next/script';
 import '../styles/global.css';
 import Layout from '../layouts/layout';
 
 export default function MyApp({ Component, pageProps }) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <>
+            <Script
+                strategy="lazyOnload"
+                src="https://www.googletagmanager.com/gtag/js?id=G-ZQ54BHNNP4"
+            />
+
+            <Script strategy="lazyOnload" id='gtag-script'>
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-ZQ54BHNNP4');
+                `}
+            </Script>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
     );
 };
